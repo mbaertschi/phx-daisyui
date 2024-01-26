a41b83132673823f5ef93524468ae9d3b209c120
Michael Bärtschi <michael.baertschi@zebbra.ch>
2024-01-26 08:40:35 +0100

    ui(secondary-layout): fix storybook example

--- storybook/layouts/secondary.story.exs
+++ storybook/layouts/secondary.story.exs
@@ -9,15 +9,32 @@ defmodule Storybook.Layouts.Secondary do
       "Sidebar navigation with sticky app-bar and main content area and an dynamic secondary sidebar on the right side."
 
   @impl true
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :show, false)
+
+    {:ok, socket}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
-    <.page current="home">
+    <.page current="home" open={@show}>
       <.header>Dashboard</.header>
+      <button type="button" class="btn" phx-click="toggle">
+        Toggle secondary
+      </button>
       <:secondary>
         <div class="bg-base-100 border-white/5 outline-black/5 min-h-screen w-80 border-l p-4 outline outline-1">
         </div>
       </:secondary>
     </.page>
     """
+  end
+
+  @impl true
+  def handle_event("toggle", _, socket) do
+    socket = update(socket, :show, &(!&1))
+
+    {:noreply, socket}
   end
 end
